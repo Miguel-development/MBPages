037f9503846afdfd4e547915ede99efe466e14f5
Miguel <miguelyovera63@gmail.com>
2021-12-02 19:54:10 -0500

    01_hamburger_finished

---
+++ main/main.js
@@ -0,0 +1,23 @@
+var d = document;
+
+d.addEventListener('DOMContentLoaded',()=>{
+    d.addEventListener("click",(e)=>{
+        if(e.target.matches("#main-hamburger") ||e.target.matches(".header-main-h2-span")) {
+            hamburger = d.querySelector("#main-hamburger-content");
+            hamburger.classList.toggle("header-main-content-hidden");
+        }
+    },false);
+
+    d.addEventListener("click",(e)=>{
+        if(e.target.matches(".header-main-content-list-items a")) {
+            hamburger = d.querySelector("#main-hamburger-content");
+            hamburger.classList.toggle("header-main-content-hidden");
+        }
+    },false);
+
+
+
+
+});
+
+
